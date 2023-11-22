c03d61db8c214c931fcd87919b3cf113430c0cc9
LisJorge <daris10jorge@gmail.com>
2023-11-21 23:36:03 -0600

    ✨ Add product authentication

--- src/products/controllers/products.controllers.ts
+++ src/products/controllers/products.controllers.ts
@@ -9,11 +9,15 @@ import {
   Param,
   Patch,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from '../services/products.service';
 import { CreateProductDto, GetAllProductsDto, UpdateProductDto } from '../dtos';
 import { Product } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards';
+import { Public } from 'src/auth/decorators';
 
+@UseGuards(JwtAuthGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
@@ -24,11 +28,13 @@ export class ProductsController {
     return this.productsService.create(createProductDto);
   }
 
+  @Public()
   @Get()
-  async findtAll(@Query() filter: GetAllProductsDto): Promise<Product[]> {
+  async findAll(@Query() filter: GetAllProductsDto): Promise<Product[]> {
     return this.productsService.findAll(filter);
   }
 
+  @Public()
   @Get(':id')
   async findOne(@Param('id') id: number) {
     return await this.productsService.findOne(id);
